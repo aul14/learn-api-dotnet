57ee96ec4a45c988bbb0a46243bdcf646e7d202e
aulia <rahmanaulia336@gmail.com>
2024-12-16 10:20:12 +0700

    sorting

--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -61,6 +61,14 @@ namespace api.Repository
                 );
             }
 
+            if (!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                if (query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
+                {
+                    stocks = query.isDescending ? stocks.OrderByDescending(s => s.Symbol) : stocks.OrderBy(s => s.Symbol);
+                }
+            }
+
             return await stocks.ToListAsync();
         }
 
